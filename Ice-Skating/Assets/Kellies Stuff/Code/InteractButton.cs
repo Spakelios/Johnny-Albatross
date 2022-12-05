b380e88d8e80eed36f6b5510727f386307383e8a
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-12-05 22:03:04 +0000

    Midi files added >:))))))

--- Ice-Skating/Assets/Kellies Stuff/Code/InteractButton.cs
+++ Ice-Skating/Assets/Kellies Stuff/Code/InteractButton.cs
@@ -8,13 +8,47 @@ public class InteractButton : MonoBehaviour
 {
     public GameObject interactButton;
     public GameObject start;
+    public AudioSource rink;
+    public AudioSource bonnie;
+    public AudioSource jeannie;
+    public AudioSource johnny;
+
+    private void Start()
+    {
+        bonnie.Stop();
+        jeannie.Stop();
+        johnny.Stop();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("NPC"))
+        if (other.CompareTag("Bonnie"))
+        {
+            interactButton.SetActive(true);
+            rink.Stop();
+            bonnie.Play();
+        }
+        
+        if (other.CompareTag("Jeannie"))
+        {
+            interactButton.SetActive(true);
+            rink.Stop();
+            jeannie.Play();
+        }
+        
+        if (other.CompareTag("Johnny"))
+        {
+            interactButton.SetActive(true);
+            rink.Stop();
+            johnny.Play();
+            
+        }
+        
+        if (other.CompareTag("Eagles"))
         {
             interactButton.SetActive(true);
         }
+        
         if(other.CompareTag("Block"))
         {
             start.SetActive(true);
@@ -25,5 +59,9 @@ public class InteractButton : MonoBehaviour
     {
         interactButton.SetActive(false);
         start.SetActive(false);
+        bonnie.Stop();
+        johnny.Stop();
+        jeannie.Stop();
+        rink.Play();
     }
 }
