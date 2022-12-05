f38793ffb3d1a72c9021adffc14c5a9c6169b30f
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-12-05 15:56:38 +0000

    Lighting makes me sad it doesnt even look any different wtf

--- Ice-Skating/Assets/startGame.cs
+++ Ice-Skating/Assets/startGame.cs
@@ -7,13 +7,26 @@ public class startGame : MonoBehaviour
 {
     public GameObject Cam2;
     public GameObject cam1;
+    public GameObject start;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Cam2.SetActive(true);
-            cam1.SetActive(false);
+            start.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (start == isActiveAndEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Cam2.SetActive(true);
+                cam1.SetActive(false);
+            }
         }
     }
 }
